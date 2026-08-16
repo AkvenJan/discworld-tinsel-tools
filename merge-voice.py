09ea96c0210b4f221871f0521a0d0bd098874af5
AkvenJan <akvenjan@gmail.com>
2026-08-16 15:51:30 +0300

    Fixed merge-voice.py errors and converted filenames from float to int for simplicity. sample-1.wav instead of sample-1.0.wav now across all scripts

--- merge-voice.py
+++ merge-voice.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 import struct
 import wave
@@ -9,11 +10,11 @@ nidxFile = open('ENGLISH-NEW.IDX', 'wb')
 count = 0
 idxFile.seek(0, 2)
 size = idxFile.tell()
-size = size / 4
+size = size // 4
 idxCount = size
 idxFile.seek(0, 0)
 written = 4
-smpFile.write('\x2e\xa1\x08\x00')
+smpFile.write(b'\x2e\xa1\x08\x00')
 while idxCount > 0:
     position = struct.unpack('<I', idxFile.read(4))[0]
     if position == 0:
@@ -22,9 +23,8 @@ while idxCount > 0:
     else:
         with open('VOICES/sample-{}.wav'.format(size - idxCount), 'rb') as wavFile:
             nidxFile.write(struct.pack('<I', written))
-            data = wavFile.read().split('data')
+            data = wavFile.read().split(b'data')
             length = struct.unpack('<I', data[1][:4])[0]
-            # smpFile.write(struct.pack('<I', length))
             smpFile.write(data[1])
             written += length + 4
     idxCount -= 1
